37fd048594bb464c69e3f1988a125404f3ccf343
Yousef <namiyousef@hotmail.com>
2023-04-13 19:27:02 +0100

    fixes bugs

--- tests/unit/test_dummy.py
+++ tests/unit/test_dummy.py
@@ -1,9 +1,10 @@
 import unittest
 
-class TestDummy(unittest.TestCase):
 
+class TestDummy(unittest.TestCase):
     def test_dummy(self):
         assert True
+
 
 if __name__ == "__main__":
     unittest.main()
